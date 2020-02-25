27b52766d22e13ba7ca759f070b4eeb69bced126
Mohd Adil <Mohd.Adil@mavenwave.local>
2020-02-25 11:24:46 +0530

    first commit

---
+++ src/main/java/com/microservice/discovery/SpringDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiscoveryApplication.class, args);
+	}
+
+}
